1c9d483d4c8ce61154620edc7544c1517a8f75bc
Arda Avcı <wonderluxct@gmail.com>
2024-05-05 17:06:39 +0300

    Create database.java

---
+++ src/database.java
@@ -0,0 +1,7 @@
+public class Database {
+    public static final String kullaniciAdi = "username";
+    public static final String parola = "password";
+    public static final String db_ismi = "database";
+    public static final String host = "localhost";
+    public static final int port = 3306;
+}
